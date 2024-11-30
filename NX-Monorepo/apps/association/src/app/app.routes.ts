351fb7782195d17b8f0cdb77ad1d24dcb9ca8ee8
Mesud Mujanovic <mesud9494@gmail.com>
2024-11-30 01:12:33 +0100

    association scss refaktor

--- NX-Monorepo/apps/association/src/app/app.routes.ts
+++ NX-Monorepo/apps/association/src/app/app.routes.ts
@@ -7,24 +7,24 @@ export const appRoutes: Route[] = [
   {
     path: 'assoc',
     loadComponent: () => import('lib/featureComponents/src/lib/shared/association/create/association.component').then(m => m.AssociationComponent)
-  }
-    // {
-    //   path: '',
-    //   loadComponent: () => import('@org/featureComponents').then((m) => m.LayoutComponent),
-    //   data: {
-    //     appTitle: appTitle,
-    //     mainNavigation: navigationConfig
-    //   },
-    //   children: [
-    //     {
-    //       path: 'create',
-    //       loadComponent: () => import('lib/featureComponents/src/lib/shared/association/create/association.component').then(m => m.AssociationComponent)
-    //     },
-    //     {
-    //       path: 'delete',
-    //       loadComponent: () => import('lib/featureComponents/src/lib/shared/association/delete/association-delete.component').then(m => m.AssociationDeleteComponent)
-    //     }
-    //   ]
-    // }
+  },
+    {
+      path: '',
+      loadComponent: () => import('@org/featureComponents').then((m) => m.LayoutComponent),
+      data: {
+        appTitle: appTitle,
+        mainNavigation: navigationConfig
+      },
+      children: [
+        {
+          path: 'create',
+          loadComponent: () => import('lib/featureComponents/src/lib/shared/association/create/association.component').then(m => m.AssociationComponent)
+        },
+        {
+          path: 'delete',
+          loadComponent: () => import('lib/featureComponents/src/lib/shared/association/delete/association-delete.component').then(m => m.AssociationDeleteComponent)
+        }
+      ]
+    }
 ]
 
--- NX-Monorepo/lib/domains/src/index.ts
+++ NX-Monorepo/lib/domains/src/index.ts
@@ -1,1 +1,2 @@
-export * from './lib/domains/domains.component';
+export * from './lib/association/interface';
+export * from './lib/my-number/interface/my-number-inteface';
